c2b57bc49e889e23b515975d68bad27db4785375
makemakeway <makemakeway@gmail.com>
2022-09-01 22:29:09 +0900

    ADD: Project Init

---
+++ TCA-Example/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  TCA-Example
+//
+//  Created by 박연배 on 2022/09/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
---
+++ TCA-Example/TCA_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_ExampleApp.swift
+//  TCA-Example
+//
+//  Created by 박연배 on 2022/09/01.
+//
+
+import SwiftUI
+
+@main
+struct TCA_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
